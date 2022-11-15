d515d6096573c94423ce8c645f9e4a1bca60d9e4
victorgimenezescribano <vgimenez@institutjaumehuguet.cat>
2022-11-15 15:59:16 +0100

    test

---
+++ app/src/main/java/com/example/activitat1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.activitat1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
